3a1e9eb4f0cdc02b4daa067e1a13114eda7de661
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-24 08:56:22 -0600

    update: Config.java
    create and include Configuration class with no bloated things only
    keeping the minimal.

---
+++ src/main/java/com/jamz/hbm/Config.java
@@ -0,0 +1,18 @@
+package com.jamz.hbm;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+@Mod.EventBusSubscriber(modid = Hbm.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    public static ForgeConfigSpec SPEC = BUILDER.build();
+
+    @SubscribeEvent
+    public static void onLoad(final ModConfigEvent event) {
+
+    }
+}
